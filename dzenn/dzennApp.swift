35ee39b50f18419cd426f1f46d2f0f81f5e03d88
BroKarim <bukutulis0110@gmail.com>
2026-02-04 14:28:19 +0700

    Initial Commit

---
+++ dzenn/dzennApp.swift
@@ -0,0 +1,17 @@
+//
+//  dzennApp.swift
+//  dzenn
+//
+//  Created by dzulkiram hilmi on 04/02/26.
+//
+
+import SwiftUI
+
+@main
+struct dzennApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
